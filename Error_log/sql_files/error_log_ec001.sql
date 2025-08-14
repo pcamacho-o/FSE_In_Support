bcfbd8b341887bdefa96bc2f97036310b99c94f0
Lalogeorgi <p.camacho@outlook.fr>
2025-08-13 22:26:33 -0400

    Error_log file to SQL

---
+++ Error_log/sql_files/error_log_ec001.sql
@@ -0,0 +1,15 @@
+CREATE TABLE error_log_ec001(
+    timestamp TIMESTAMP,
+    error_code VARCHAR(55),
+    error_type TEXT,
+    description TEXT,
+    severity VARCHAR(55),
+    firmware_version INT,
+    temperature INT,
+    operator VARCHAR(55),
+
+COPY error
+
+
+
+
