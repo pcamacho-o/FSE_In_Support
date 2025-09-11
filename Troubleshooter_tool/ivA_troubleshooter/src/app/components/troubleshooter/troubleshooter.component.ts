81b1961bd829c5900433107e5e27ace0b0555d44
Lalogeorgi <p.camacho@outlook.fr>
2025-09-11 20:26:09 -0300

    migrate to standalone

--- Troubleshooter_tool/ivA_troubleshooter/src/app/components/troubleshooter/troubleshooter.component.ts
+++ Troubleshooter_tool/ivA_troubleshooter/src/app/components/troubleshooter/troubleshooter.component.ts
@@ -1,9 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FlowService, FlowData, Symptom, Step } from '../../services/flow.service';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+// Angular Material imports
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
 @Component({
   selector: 'ivA_troubleshooter',
   standalone: true,
+  imports: [ CommonModule,
+    FormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatToolbarModule
+  ], 
   templateUrl: './troubleshooter.component.html',
   styleUrls: ['./troubleshooter.component.scss']
 })
